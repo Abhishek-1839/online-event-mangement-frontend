bc728d23823bc83d239f16eb8db5b8e16e953e98
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2024-09-26 20:19:04 +0530

    gncg

--- src/data/api.js
+++ src/data/api.js
@@ -10,4 +10,17 @@ const api = axios.create
   }
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.data.error === 'Token expired. Please log in again.') {
+      // Clear the token from localStorage
+      localStorage.removeItem('token');
+      // Redirect to login page
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
